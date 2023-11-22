5b069e3c85a8efe61ce833b927a9136f099fe7a3
Dorasan <tuandung6a5@gmail.com>
2023-11-22 16:48:26 +0700

    fix modules

--- src/Frontend/aurasky-fe/pages/home/components/highlight.tsx
+++ src/Frontend/aurasky-fe/pages/home/components/highlight.tsx
@@ -1,9 +1,27 @@
 import { useEffect, useState } from "react";
 import { HOST } from "../../../utils/constant";
-import CardNFT from "./card-nfts";
-import Loading from "./loading";
+
 
 export default function Highlight() {
     const [error, setError] = useState<any | null>(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
+
+    //TODO: fix modules
+
+    useEffect(() => {
+        fetch(`${HOST}/nft/get-all`)
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, []);
+}
+//reorganized, delete later
