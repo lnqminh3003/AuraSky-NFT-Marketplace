c730b1ee80b6e71c8270914c4c28a5fb97532341
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2023-11-23 14:53:23 +0700

    window navigation bar

---
+++ src/Frontend/aurasky-fe/components/navigationBar.tsx
@@ -0,0 +1,26 @@
+import createKeccakHash from "keccak";
+import { KeyboardEvent, useEffect,  useState } from "react";
+import WalletConnect from "../pages/home/components/walletConnect";
+import Link from "next/link";
+import Account from "../pages/home/components/account";
+import GetMoney from "../pages/home/components/getMoney";
+import Router, { useRouter } from "next/router";
+import web3 from "../helpers/connectMetamask/web3";
+
+declare let window: any;
+
+const NavigationBar = (searchQuery: any) => {
+  const router = useRouter();
+  // console.log("searching ", searchQuery["searchQuery"]);
+
+  const [searchValue, setSearchValue] = useState(
+    searchQuery["searchQuery"] == undefined
+      ? undefined
+      : searchQuery["searchQuery"]
+  );
+  const [isSearching, setIsSearching] = useState(false);
+  const [isDropOpen, setDrop] = useState({
+    explore: false,
+    profile: false,
+    wallet: false,
+  });
