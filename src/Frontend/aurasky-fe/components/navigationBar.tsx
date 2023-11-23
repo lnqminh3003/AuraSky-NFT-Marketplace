eeadbe7f0a23c5356ab73f7d49dedd117332a6b9
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2023-11-23 14:55:31 +0700

    check sum address

--- src/Frontend/aurasky-fe/components/navigationBar.tsx
+++ src/Frontend/aurasky-fe/components/navigationBar.tsx
@@ -24,3 +24,46 @@ const NavigationBar = (searchQuery: any) => {
     profile: false,
     wallet: false,
   });
+
+  const [defaultAccount, setDefaultAccount] = useState("");
+  const [userBalance, setUserBalance] = useState("");
+  const [connectMessage, setConnectMessage] = useState("Connect with MetaMask");
+  const [showModal, setShowModal] = useState(false);
+  const [modalMetamask, setModalMetamask] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("account") != "") {
+      ConnectToMetamask();
+    }
+  }, []);
+
+  const ConnectToMetamask = async () => {
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      await window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((result: any[]) => {
+          localStorage.setItem("account", toChecksumAddress(result[0]));
+          setDefaultAccount(toChecksumAddress(result[0]));
+          accountChangeHandler(result);
+        });
+    } else {
+      setModalMetamask(true);
+      console.log("Install Metamask");
+    }
+  };
+
+  function toChecksumAddress(address: string) {
+    address = address.toLowerCase().replace("0x", "");
+    var hash = createKeccakHash("keccak256").update(address).digest("hex");
+    var ret = "0x";
+
+    for (var i = 0; i < address.length; i++) {
+      if (parseInt(hash[i], 16) >= 8) {
+        ret += address[i].toUpperCase();
+      } else {
+        ret += address[i];
+      }
+    }
+
+    return ret;
+  }
