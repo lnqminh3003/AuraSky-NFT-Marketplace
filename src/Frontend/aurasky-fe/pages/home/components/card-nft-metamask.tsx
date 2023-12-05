01a2388aa017f1a2189bb8542176d833b7f0e11d
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-12-05 12:08:12 +0700

    Update card-nft-metamask.tsx

--- src/Frontend/aurasky-fe/pages/home/components/card-nft-metamask.tsx
+++ src/Frontend/aurasky-fe/pages/home/components/card-nft-metamask.tsx
@@ -33,3 +33,22 @@ var ImageURL = [
     .catch((error) => {
       console.log(error);
     });
+    return (
+      <Link href={{ pathname: "./nft", query: { id: id } }}>
+        <div className="relative flex-wrap w-60 h-60 inline-block">
+          <img
+            src= {image}
+            alt=""
+            className=" w-60 h-60 object-cover rounded-3xl"
+          />
+          <div className="absolute right-0 bottom-0 left-0 w-full h-32 overflow-hidden bg-gradient-to-t from-black opacity-75 rounded-b-3xl"></div>
+          {/* <div className="absolute right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-gradient-to-t from-black rounded-b-3xl"></div> */}
+          <div className="w-auto h-12 absolute bottom-8 left-3 right-3 rounded-xl p-1 px-3">
+            <div className="text-2xl font-bold text-white truncate">
+              ID: {id}
+            </div>
+          </div>
+        </div>
+      </Link>
+    );
+  }
