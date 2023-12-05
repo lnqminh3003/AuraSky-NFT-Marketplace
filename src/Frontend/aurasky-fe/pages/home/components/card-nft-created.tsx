3bdf41710985c7764b05d1c86b3787b01a924f3b
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-12-05 12:04:39 +0700

    Update card-nft-created.tsx

--- src/Frontend/aurasky-fe/pages/home/components/card-nft-created.tsx
+++ src/Frontend/aurasky-fe/pages/home/components/card-nft-created.tsx
@@ -1,4 +1,12 @@
 import Link from "next/link";
 import lodash from "lodash";
-
+function CardNFTCreated({ id, nft }: { id: string; nft: any }) {
+  return (
+    <Link href={{ pathname: "./sell", query: { id:id } }}>
+      <div className="relative flex-wrap w-60 h-60 inline-block">
+        <img
+          src={lodash.get(nft, "contentUrl")}
+          alt=""
+          className=" w-60 h-60 object-cover rounded-3xl"
+        />
 export default CardNFTCreated;
