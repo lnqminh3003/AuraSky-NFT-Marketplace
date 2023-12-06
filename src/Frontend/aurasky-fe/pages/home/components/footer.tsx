847036773ee0cd6c171f066b1945325c7e0b15cf
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-12-06 20:06:01 +0700

    Update footer.tsx

--- src/Frontend/aurasky-fe/pages/home/components/footer.tsx
+++ src/Frontend/aurasky-fe/pages/home/components/footer.tsx
@@ -1,1 +1,15 @@
 
+export default function Footer() {
+  return (
+      <div className="bg-cyan-600 p-16 mt-32 px-32">
+          <div className="flex items-center">
+              <div>
+                  <div className="flex items-center">
+                      <img
+                          src="https://i.imgur.com/98gX8Ky.png"
+                          className="h-6 mr-3 sm:h-9"
+                      />
+                      <span className="self-center text-3xl font-semibold whitespace-nowrap text-white">
+                          AuraSky
+                      </span>
+                  </div>
