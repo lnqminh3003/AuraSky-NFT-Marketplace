f3b15cffbf5c88709c57f3214ecb00a5fcf8e989
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-12-06 20:10:39 +0700

    Create userModel.ts

---
+++ src/Backend/src/models/userModel.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String },
+  bio: { type: String },
+  email: { type: String },
+  profilePicUrl: { type: String },
+  address: { type: String },
+});
