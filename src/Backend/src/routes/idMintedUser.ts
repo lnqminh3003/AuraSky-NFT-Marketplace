968e3ff7a352204b42e4531a2443978106aee457
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-12-07 13:58:49 +0700

    Update idMintedUser.ts

--- src/Backend/src/routes/idMintedUser.ts
+++ src/Backend/src/routes/idMintedUser.ts
@@ -14,3 +14,19 @@ idMintedRoute.get("/get", async (req, res) => {
       res.status(500).json(err);
   }
 });
+
+idMintedRoute.post("/update", async (req, res) => {
+  try {
+      log("update id minted", req.body);
+      const idMinted = await idMintedModel.findByIdAndUpdate(
+          "63a1d0b22c029b1c8024846b",
+          req.body,
+          {
+              returnDocument: "after",
+          }
+      );
+      res.json(idMinted);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
