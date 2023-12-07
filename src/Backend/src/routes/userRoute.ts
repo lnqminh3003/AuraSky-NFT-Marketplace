f5ba69d350e2f3912ee15441185ded8ad2bc41c6
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-12-07 13:35:24 +0700

    Update userRoute.ts

--- src/Backend/src/routes/userRoute.ts
+++ src/Backend/src/routes/userRoute.ts
@@ -4,3 +4,14 @@ import userModel from "../models/userModel";
 import log from "../utils/log";
 
 const userRoute = Router();
+
+userRoute.put("/create", async (req, res) => {
+  try {
+    log("create user", req.body);
+
+    const usr = await userModel.create(req.body);
+    res.json(usr.toObject);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
