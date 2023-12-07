234d2403b68a430a8949213e1d238c368c4de2a3
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-12-07 13:58:38 +0700

    Update idMintedUser.ts

--- src/Backend/src/routes/idMintedUser.ts
+++ src/Backend/src/routes/idMintedUser.ts
@@ -3,3 +3,14 @@ import idMintedModel from "../models/idMintedModel";
 import log from "../utils/log";
 
 const idMintedRoute = Router();
+
+idMintedRoute.get("/get", async (req, res) => {
+  try {
+      log("get all id minted");
+
+      const ids = await idMintedModel.find({});
+      res.json(ids);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
