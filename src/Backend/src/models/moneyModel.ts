1b6c41bd99c66ea1501faa701dceea63db798f69
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2023-12-12 10:31:17 +0700

    Add NFT Model and Update some Model in BE

--- src/Backend/src/models/moneyModel.ts
+++ src/Backend/src/models/moneyModel.ts
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
-
 const moneySchema = new mongoose.Schema({
     ownerAddress: { type: String , require:true},
     money : {type :String, require : true}
 });
-
 export default mongoose.model("money", moneySchema);
--- src/Backend/src/models/nftModel.ts
+++ src/Backend/src/models/nftModel.ts
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
-const nftCreateSchema = new mongoose.Schema({
+
+const nftSchema = new mongoose.Schema({
   name: { type: String, required: true },
   contentUrl: { type: String, required: true },
   description: { type: String },
   idNFT: { type: String, required: true },
+  price: { type: String },
   ownerAddress: { type: String },
 });
-export default mongoose.model("NFTCreate", nftCreateSchema);
+
+nftSchema.index({ name: "text" });
+
+export default mongoose.model("NFTs", nftSchema);
