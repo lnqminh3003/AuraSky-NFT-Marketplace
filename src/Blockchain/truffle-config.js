e294606f8fb97fed9f94be073ede99ee7fbd4c1b
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2024-01-03 22:22:50 +0700

    Add build Math and edit Sol version

--- src/Blockchain/truffle-config.js
+++ src/Blockchain/truffle-config.js
@@ -5,12 +5,6 @@ const mnemonic = require("./secrets.json").mnemonic;
 
 module.exports = {
   networks: {
-    bsc: {
-      provider: () =>
-        new HDWalletProvider(mnemonic, "https://bsc-dataseed.binance.org/"),
-      network_id: 56,
-      skipDryRun: true,
-    },
     testnet: {
       provider: () => new HDWalletProvider(mnemonic, `https://floral-fluent-needle.bsc-testnet.quiknode.pro/b488701420b4a082279df007a6275836c1883a64/`),
       network_id: 97,
@@ -25,7 +19,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.8.0" 
+      version: "0.8.1" 
     }
   }
 };
