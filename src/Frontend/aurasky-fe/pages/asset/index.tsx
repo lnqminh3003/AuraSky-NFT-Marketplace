8cec373ae9e9079b2fb8193f052b4158c7037351
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2024-01-04 18:34:09 +0700

    front end black background

--- src/Frontend/aurasky-fe/pages/asset/index.tsx
+++ src/Frontend/aurasky-fe/pages/asset/index.tsx
@@ -225,7 +225,7 @@ export default function NFTPage() {
                       NFT in Metamask
                     </Typography>
                   </div>
-                  <button onClick={GetNFT} className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  <button onClick={GetNFT} className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-black-500 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                       Get NFT in Metamask
                   </button>
                   
