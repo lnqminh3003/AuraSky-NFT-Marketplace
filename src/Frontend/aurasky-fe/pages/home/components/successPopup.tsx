6580edf2bb783d72af60d726eb4e2d024fd99953
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2024-01-04 18:35:29 +0700

    simplify

--- src/Frontend/aurasky-fe/pages/home/components/successPopup.tsx
+++ src/Frontend/aurasky-fe/pages/home/components/successPopup.tsx
@@ -6,16 +6,6 @@ const SuccessPopup = () => {
     <div>
       <div className="grid place-items-center bg-black bg-opacity-60 fixed top-0 left-0 right-0 z-50 w-full p-4 overflw-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 w-96 grid place-items-center">
-          <div className="flex items-start p-4 border-b rounded-t dark:border-gray-600">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/148/148767.png"
-              className="p-1 rounded h-11 w-11"
-              alt="..."
-            />
-            <h3 className="text-xl font-semibold pt-2 pl-4 text-gray-900 dark:text-white">
-              Payment successful
-            </h3>
-          </div>
           <div className="p-6 space-y-6">
             <p className="font-semibold text-base leading-relaxed text-gray-500 dark:text-gray-400">
               THANK YOU
