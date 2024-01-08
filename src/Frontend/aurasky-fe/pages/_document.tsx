a435e0f5250abe6dd712f19885e79ea9006249a8
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2024-01-08 08:25:34 +0700

    Fix get Money and Loading

--- src/Frontend/aurasky-fe/pages/_document.tsx
+++ src/Frontend/aurasky-fe/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel = "icon" href = "https://i.imgur.com/98gX8Ky.png" type = "image/x-icon"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
       </Head>
       <body>
--- src/Frontend/aurasky-fe/pages/home/components/loading.tsx
+++ src/Frontend/aurasky-fe/pages/home/components/loading.tsx
@@ -1,16 +1,39 @@
-const Loading = () => {
-  return <div>
-    <div className="bg-blue-300 justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-      <div className="relative w-auto my-6 mx-auto max-w-3xl">
-        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-blue-400 outline-none focus:outline-none">
-          <div className="relative p-6 flex-auto items-center">
-            <p className="text-2xl font-bold whitespace-nowrap dark:text-white">LOADING</p>
+import { useEffect, useState } from "react";
+
+const SuccessPopup = () => {
+  const [modalSuccess, setModalSuccess] = useState(false);
+  return (
+    <div>
+      <div className="grid place-items-center bg-black bg-opacity-60 fixed top-0 left-0 right-0 z-50 w-full p-4 overflw-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 w-96 grid place-items-center">
+          <div className="flex items-start p-4 border-b rounded-t dark:border-gray-600">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/148/148767.png"
+              className="p-1 rounded h-11 w-11"
+              alt="..."
+            />
+            <h3 className="text-xl font-semibold pt-2 pl-4 text-gray-900 dark:text-white">
+              Payment successful
+            </h3>
+          </div>
+          <div className="p-6 space-y-6">
+            <p className="font-semibold text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              THANK YOU
+            </p>
+          </div>
+          <div className="flex items-center p-6 space-x-2  rounded-b dark:border-gray-600">
+            <button
+              data-modal-toggle="defaultModal"
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Let's go
+            </button>
           </div>
         </div>
       </div>
     </div>
-    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-  </div>
-}
+  );
+};
 
-export default Loading
+export default SuccessPopup;
