4ef5e054cc92e9fb7681db63acabbdc31a4653f5
Dorasan <tuandung6a5@gmail.com>
2024-01-08 09:50:50 +0700

    fixed connection

--- src/Frontend/aurasky-fe/pages/home/components/loading.tsx
+++ src/Frontend/aurasky-fe/pages/home/components/loading.tsx
@@ -1,5 +1,5 @@
-const Loading = () => {
-  return <div>
+const Loading =()=>{
+    return <div>
     <div className="bg-blue-300 justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
       <div className="relative w-auto my-6 mx-auto max-w-3xl">
         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-blue-400 outline-none focus:outline-none">
@@ -10,7 +10,7 @@ const Loading = () => {
       </div>
     </div>
     <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-  </div>
+</div>
 }
 
 export default Loading
