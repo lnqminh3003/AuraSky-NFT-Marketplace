83ce084a92f638c1311de865c1d1a1def460e310
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2024-01-09 20:54:03 +0700

    moving

--- src/Blockchain/AuraSky NFT/truffle-config.js
+++ src/Blockchain/AuraSky NFT/truffle-config.js
@@ -5,6 +5,12 @@ const mnemonic = require("./secrets.json").mnemonic;
 
 module.exports = {
   networks: {
+    bsc: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, "https://bsc-dataseed.binance.org/"),
+      network_id: 56,
+      skipDryRun: true,
+    },
     testnet: {
       provider: () => new HDWalletProvider(mnemonic, `https://floral-fluent-needle.bsc-testnet.quiknode.pro/b488701420b4a082279df007a6275836c1883a64/`),
       network_id: 97,
@@ -19,7 +25,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.8.1" 
+      version: "0.8.20" 
     }
   }
 };
